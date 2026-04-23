8805a24ec1c9869157dee3bad90d76048db1f4b8
karishmamedapati84-beep <karishmamedapati84@gmail.com>
2026-04-23 20:18:25 +0530

    Add files via upload

---
+++ Task3.js
@@ -0,0 +1,9 @@
+let value="123"
+let boolean="true"
+let number="55"
+let str1="abc"
+
+let isNumber=Number(value)
+let str = (typeof boolean)
+let isstring = (tostring(pqr))
+let isStr1=Number(str1)
---
+++ Task4.js
@@ -0,0 +1,4 @@
+let string="10"
+let number=5
+
+console.log(string+number)
