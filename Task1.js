e925a00641fc0c52635bdaf0c14f292f326a6410
karishmamedapati84-beep <karishmamedapati84@gmail.com>
2026-04-23 20:13:32 +0530

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,7 @@
+var myName ="M.Karishma"
+let age= 18
+const collegeName ="Gandhi Institute Of Engineering and Technology University"
+
+console.log(myName)
+console.log(age)
+console.log(collegeName)
---
+++ Task2.js
@@ -0,0 +1,10 @@
+num=10
+str="Javascript"
+isStudent =true
+nothing = null
+undefined
+
+console.log(typeof num)
+console.log(typeof str)
+console.log(typeof isStudent)
+console.log(typeof nothing)
