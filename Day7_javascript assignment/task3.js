eccb1542c07b26afc18967b415392ef97abefe8f
karishmamedapati84-beep <karishmamedapati84@gmail.com>
2026-05-02 07:32:18 +0530

    Add files via upload

---
+++ Day7_javascript assignment/task3.js
@@ -0,0 +1,18 @@
+let i= 1;
+while(i<=10){
+    console.log(i)
+    i++
+}
+
+let j=10;
+while(j>=1){
+    console.log(j)
+    j--
+}
+
+let a= 11
+do{
+    console.log(a)
+    a++
+}
+while(a <= 10)
---
+++ Day7_javascript assignment/task4.js
@@ -0,0 +1,4 @@
+const heroes = ["Ironman", "spiderman","Thor", "Hulk"]
+    for (let i = 0; i <heroes.length; i++){
+    console.log(heroes[i])
+}
