7998a0d9919edf0b28a5b6c6867375e096357d8c
karishmamedapati84-beep <karishmamedapati84@gmail.com>
2026-05-08 22:16:44 +0530

    Add files via upload

---
+++ 1_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail ="karishmamedapati84@gmail.com"
+var accountPassword ="12345"
+accountcity = "Hderabad"
+let accountState
+
+// accountId =2    //not allowed
+
+accountEmail ="abc@gmail.com"
+accountPassword ="543568"
+accountcity ="Hyderabad"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState])
