fb11a4572171bb183e32b9633dd154fa73ae0c56
karishmamedapati84-beep <karishmamedapati84@gmail.com>
2026-05-09 21:08:38 +0530

    Add files via upload

---
+++ 2_Datatype.js
@@ -0,0 +1,12 @@
+// alert(4+4) // we are using nodejs, not browser
+
+let name ="Karishma"
+let age= 18
+let isLoggedIn = false
+let state
+
+console.log(typeof "GIET")
+console.log(typeof age)
+// interview quetion
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ 3_conversion.js
@@ -0,0 +1,19 @@
+let score="33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+// comment it 3lines
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+/*
+"33abc" => NaN
+true =>1; false =>0;
+*/
+
+let isLoggedIn ="Karishma"
+
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn)
