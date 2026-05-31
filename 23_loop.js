d14209626836d45839f2cf02a7dda1cc8f42295c
karishmamedapati84-beep <karishmamedapati84@gmail.com>
2026-05-31 21:44:38 +0530

    Add files via upload

---
+++ 23_loop.js
@@ -0,0 +1,18 @@
+const myObject = {
+    js : "javascript",
+    cpp : "c++",
+    rb : "ruby",
+    swift : "swift by apple"
+}
+
+for(const key in myObject){
+    //console.log(myobject[key])
+    console.log(`${key} shortcut is for ${myObject[key]}`)
+}
+
+//********************Array for in***************
+const programming = ["js", "rb", "py", "java", "cpp"]
+for(const key in programming){
+    //console.log(key)
+    console.log(programming[key])
+}
---
+++ 24_foreach.js
@@ -0,0 +1,13 @@
+//Important for job interview
+//see the difference between for and foreach
+const coding = ["js", "rb", "py", "java", "cpp"]
+
+coding.forEach(function(item){
+    console.log(item)
+})
+
+
+//**********************By using arrow function****************8
+coding.forEach((item) => {
+    console.log(item)
+})
